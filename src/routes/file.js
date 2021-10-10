12344f36f282f831cfca042c77da4509bdaaa3ea
Lyxas <lucasgazeau@lgazeau.fr>
2021-10-10 13:41:02 +0200

    add character

--- src/routes/file.js
+++ src/routes/file.js
@@ -2,7 +2,7 @@ module.exports = function(app, path)
 {
     app.get(path+"/getVideo/:name",(req,res) =>{
         console.log(req.params.name)
-        const checkForWord = RegExp('^[a-zA-Z]*$');
+        const checkForWord = RegExp('^[a-zA-Zéêèçôîûâ]*$');
         if(!checkForWord.exec(req.params.name))
             res.status(400).send("Invalid Request")
         res.sendFile(process.env.MEDIA_PATH+req.params.name+".mp4", {root : process.env.PWD})
